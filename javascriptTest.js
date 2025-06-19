1cdd59760f443801e8c38a14a58755aed31e135b
danbe8080 <danbe8080@gmail.com>
2025-06-19 19:02:01 +0900

    프로그래머스 강의 정리 - 타입스크립트란 - 타입스크립트와 자바스크립트의 차이점 - 타입스크립트를 자바스크립트로 컴파일 실행 - 타입스크립트 초기 셋팅 파일 생성

---
+++ javascriptTest.js
@@ -0,0 +1,11 @@
+// 자바스크립트
+
+// a, b 덧셈하고 결과물을 리턴 함수
+
+function plusJs(a, b) {
+    return a + b;
+}
+
+console.log(plusJs(3, 5)); // 8
+
+console.log(plusJs('3','5')); // 35
---
+++ typescriptTest.ts
@@ -0,0 +1,10 @@
+// 타입스크립트
+
+// a, b 덧셈하고 결과물을 리턴 함수
+
+function plusTs(a: number, b : number) {
+    return a + b;
+}
+
+console.log(plusTs(3, 5)); // 8
+// console.log(plus('3','5')); => Error 발생
